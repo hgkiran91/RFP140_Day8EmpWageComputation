5a178d1553bc802abd18cf7d1ed1af06b4b5f8a3
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-28 20:07:42 +0530

    Added: UC1

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,5 +3,13 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int empPresent = 1;
+        int empCheck = (int) (Math.random() * 10) % 2;
+        if (empCheck == empPresent) {
+            System.out.println("Employee Present");
+        } else {
+            System.out.println("Employee Absent");
+        }
     }
 }
